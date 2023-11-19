b7f4eaa492a56b89df18af03a3ff835d2cec562f
William Emerison Six <billsix@gmail.com>
2023-11-19 17:56:20 -0500

    clang-format

--- Chapter12/ch12Animation.cpp
+++ Chapter12/ch12Animation.cpp
@@ -4,106 +4,97 @@
 
 const double TWO_PI = 6.2831853;
 
-GLsizei winWidth = 500, winHeight = 500;    // Initial display window size.
+GLsizei winWidth = 500, winHeight = 500;  // Initial display window size.
 GLuint regHex;                            //  Define name for display list.
 static GLfloat rotTheta = 0.0;
 
 class scrPt {
-public:
-   GLint x, y;
+ public:
+  GLint x, y;
 };
 
-static void init (void)
-{
-   scrPt hexVertex;
-   GLdouble hexTheta;
-   GLint k;
+static void init(void) {
+  scrPt hexVertex;
+  GLdouble hexTheta;
+  GLint k;
 
-   glClearColor (1.0, 1.0, 1.0, 0.0);
+  glClearColor(1.0, 1.0, 1.0, 0.0);
 
-   /*  Set up a display list for a red regular hexagon.
-    *  Vertices for the hexagon are six equally spaced
-    *  points around the circumference of a circle.
-    */
-   regHex = glGenLists (1);
-   glNewList (regHex, GL_COMPILE);
-      glColor3f (1.0, 0.0, 0.0);
-      glBegin (GL_POLYGON);
-         for (k = 0; k < 6; k++) {
-            hexTheta = TWO_PI * k / 6;
-            hexVertex.x = 150 + 100 * cos (hexTheta);
-            hexVertex.y = 150 + 100 * sin (hexTheta);
-            glVertex2i (hexVertex.x, hexVertex.y);
-         }
-      glEnd ( );
-   glEndList ( );
+  /*  Set up a display list for a red regular hexagon.
+   *  Vertices for the hexagon are six equally spaced
+   *  points around the circumference of a circle.
+   */
+  regHex = glGenLists(1);
+  glNewList(regHex, GL_COMPILE);
+  glColor3f(1.0, 0.0, 0.0);
+  glBegin(GL_POLYGON);
+  for (k = 0; k < 6; k++) {
+    hexTheta = TWO_PI * k / 6;
+    hexVertex.x = 150 + 100 * cos(hexTheta);
+    hexVertex.y = 150 + 100 * sin(hexTheta);
+    glVertex2i(hexVertex.x, hexVertex.y);
+  }
+  glEnd();
+  glEndList();
 }
 
-void displayHex (void)
-{
-   glClear (GL_COLOR_BUFFER_BIT);
+void displayHex(void) {
+  glClear(GL_COLOR_BUFFER_BIT);
 
-   glPushMatrix ( );
-   glRotatef (rotTheta, 0.0, 0.0, 1.0);
-   glCallList (regHex);
-   glPopMatrix ( );
+  glPushMatrix();
+  glRotatef(rotTheta, 0.0, 0.0, 1.0);
+  glCallList(regHex);
+  glPopMatrix();
 
-   glutSwapBuffers ( );
+  glutSwapBuffers();
 
-   glFlush ( );
+  glFlush();
 }
 
-void rotateHex (void)
-{
-   rotTheta += 3.0;
-   if (rotTheta > 360.0)
-      rotTheta -= 360.0;
+void rotateHex(void) {
+  rotTheta += 3.0;
+  if (rotTheta > 360.0) rotTheta -= 360.0;
 
-   glutPostRedisplay ( );
+  glutPostRedisplay();
 }
 
-void winReshapeFcn (GLint newWidth, GLint newHeight)
-{
-   glViewport (0, 0, (GLsizei) newWidth, (GLsizei) newHeight);
+void winReshapeFcn(GLint newWidth, GLint newHeight) {
+  glViewport(0, 0, (GLsizei)newWidth, (GLsizei)newHeight);
 
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ( );
-   gluOrtho2D (-320.0, 320.0, -320.0, 320.0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-320.0, 320.0, -320.0, 320.0);
 
-   glMatrixMode (GL_MODELVIEW);
-   glLoadIdentity ( );
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 
-   glClear (GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void mouseFcn (GLint button, GLint action, GLint x, GLint y)
-{
-   switch (button) {
-      case GLUT_MIDDLE_BUTTON:           //  Start the rotation.
-         if (action == GLUT_DOWN)
-            glutIdleFunc (rotateHex);
-         break;
-      case GLUT_RIGHT_BUTTON:            //  Stop the rotation.
-         if (action == GLUT_DOWN)
-            glutIdleFunc (NULL);
-         break;
-      default:
-         break;
-   }
+void mouseFcn(GLint button, GLint action, GLint x, GLint y) {
+  switch (button) {
+    case GLUT_MIDDLE_BUTTON:  //  Start the rotation.
+      if (action == GLUT_DOWN) glutIdleFunc(rotateHex);
+      break;
+    case GLUT_RIGHT_BUTTON:  //  Stop the rotation.
+      if (action == GLUT_DOWN) glutIdleFunc(NULL);
+      break;
+    default:
+      break;
+  }
 }
 
-void main (int argc, char** argv)
-{
-   glutInit (&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowPosition (150, 150);
-   glutInitWindowSize (winWidth, winHeight);
-   glutCreateWindow ("Animation Example");
+void main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowPosition(150, 150);
+  glutInitWindowSize(winWidth, winHeight);
+  glutCreateWindow("Animation Example");
 
-   init ( );
-   glutDisplayFunc (displayHex);
-   glutReshapeFunc (winReshapeFcn);
-   glutMouseFunc (mouseFcn);
+  init();
+  glutDisplayFunc(displayHex);
+  glutReshapeFunc(winReshapeFcn);
+  glutMouseFunc(mouseFcn);
 
-   glutMainLoop ( );
+  glutMainLoop();
 }
--- Chapter13/ch13Polyhedron.cpp
+++ Chapter13/ch13Polyhedron.cpp
@@ -2,68 +2,64 @@
 
 GLsizei winWidth = 500, winHeight = 500;  // Initial display-window size.
 
-void init (void)
-{
-   glClearColor (1.0, 1.0, 1.0, 0.0);  // White display window.
+void init(void) {
+  glClearColor(1.0, 1.0, 1.0, 0.0);  // White display window.
 }
 
-void displayWirePolyhedra (void)
-{
-   glClear (GL_COLOR_BUFFER_BIT);  // Clear display window.
+void displayWirePolyhedra(void) {
+  glClear(GL_COLOR_BUFFER_BIT);  // Clear display window.
 
-   glColor3f (0.0, 0.0, 1.0);      // Set line color to blue.
+  glColor3f(0.0, 0.0, 1.0);  // Set line color to blue.
 
-   /*  Set viewing transformation.  */
-   gluLookAt (5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  /*  Set viewing transformation.  */
+  gluLookAt(5.0, 5.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-   /*  Scale cube and display as wire-frame parallelepiped. */
-   glScalef (1.5, 2.0, 1.0);
-   glutWireCube (1.0);
+  /*  Scale cube and display as wire-frame parallelepiped. */
+  glScalef(1.5, 2.0, 1.0);
+  glutWireCube(1.0);
 
-   /*  Scale, translate, and display wire-frame dodecahedron. */
-   glScalef (0.8, 0.5, 0.8);
-   glTranslatef (-6.0, -5.0, 0.0);
-   glutWireDodecahedron ( );
+  /*  Scale, translate, and display wire-frame dodecahedron. */
+  glScalef(0.8, 0.5, 0.8);
+  glTranslatef(-6.0, -5.0, 0.0);
+  glutWireDodecahedron();
 
-   /*  Translate and display wire-frame tetrahedron. */
-   glTranslatef (8.6, 8.6, 2.0);
-   glutWireTetrahedron ( );
+  /*  Translate and display wire-frame tetrahedron. */
+  glTranslatef(8.6, 8.6, 2.0);
+  glutWireTetrahedron();
 
-   /*  Translate and display wire-frame octahedron. */
-   glTranslatef (-3.0, -1.0, 0.0);
-   glutWireOctahedron ( );
+  /*  Translate and display wire-frame octahedron. */
+  glTranslatef(-3.0, -1.0, 0.0);
+  glutWireOctahedron();
 
-   /*  Scale, translate, and display wire-frame icosahedron. */
-   glScalef (0.8, 0.8, 1.0);
-   glTranslatef (4.3, -2.0, 0.5);
-   glutWireIcosahedron ( );
+  /*  Scale, translate, and display wire-frame icosahedron. */
+  glScalef(0.8, 0.8, 1.0);
+  glTranslatef(4.3, -2.0, 0.5);
+  glutWireIcosahedron();
 
-   glFlush ( );
+  glFlush();
 }
 
-void winReshapeFcn (GLint newWidth, GLint newHeight)
-{
-   glViewport (0, 0, newWidth, newHeight);
+void winReshapeFcn(GLint newWidth, GLint newHeight) {
+  glViewport(0, 0, newWidth, newHeight);
 
-   glMatrixMode (GL_PROJECTION);
-   glFrustum (-1.0, 1.0, -1.0, 1.0, 2.0, 20.0);
+  glMatrixMode(GL_PROJECTION);
+  glFrustum(-1.0, 1.0, -1.0, 1.0, 2.0, 20.0);
 
-   glMatrixMode (GL_MODELVIEW);
+  glMatrixMode(GL_MODELVIEW);
 
-   glClear (GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void main (int argc, char** argv)
-{
-   glutInit (&argc, argv);
-   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-   glutInitWindowPosition (100, 100);
-   glutInitWindowSize (winWidth, winHeight);
-   glutCreateWindow ("Wire-Frame Polyhedra");
+void main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(winWidth, winHeight);
+  glutCreateWindow("Wire-Frame Polyhedra");
 
-   init ( );
-   glutDisplayFunc (displayWirePolyhedra);
-   glutReshapeFunc (winReshapeFcn);
+  init();
+  glutDisplayFunc(displayWirePolyhedra);
+  glutReshapeFunc(winReshapeFcn);
 
-   glutMainLoop ( );
+  glutMainLoop();
 }
