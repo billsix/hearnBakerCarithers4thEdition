47c9989542dd19e95a63e68a3e79a5f02abba3f5
William Emerison Six <billsix@gmail.com>
2023-11-19 17:57:54 -0500

    another example from chapter 4

--- Chapter4/ch4DispLstReshape.cpp
+++ Chapter4/ch4DispLstReshape.cpp
@@ -70,7 +70,7 @@ void winReshapeFcn(int newWidth, int newHeight) {
   glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void main(int argc, char** argv) {
+int main(int argc, char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowPosition(100, 100);
